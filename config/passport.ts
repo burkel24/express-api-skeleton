35a6716352e26430385a036f543cada4617ea409
Burke Livingston <burkel24@gmail.com>
2018-02-03 13:15:49 -0500

    Set up build and basic app structure

---
+++ config/passport.ts
@@ -0,0 +1,53 @@
+// const LocalStrategy = require('passport-local').Strategy;
+// const FacebookStrategy = require('passport-facebook').Strategy;
+
+// const User = require('../models/user');
+
+export default function configurePassport(passport) {
+
+  // passport.serializeUser(function(user, done) {
+  //   done(null, user.id);
+  // });
+
+  // passport.deserializeUser(function(id, done) {
+  //   User.findById(id, function(err, user) {
+  //     done(err, user);
+  //   });
+  // });
+
+  // passport.use(new FacebookStrategy({
+  //   clientID: process.env.FACEBOOK_CLIENT_ID,
+  //   clientSecret: process.env.FACEBOOK_SECRET,
+  //   callbackURL: process.env.FACEBOOK_CALLBACK
+  // },
+  // function(token, refreshToken, profile, done) {
+  //   process.nextTick(function() {
+  //     User.findOne({ 'facebook.id' : profile.id }, function(err, user) {
+  //       if (err) return done(err);
+
+
+  //       if (user) {
+  //         return done(null, user);
+  //       } else {
+  //         const newUser  = new User();
+
+  //         newUser.facebook.id    = profile.id;
+  //         newUser.facebook.token = token;
+
+  //         if (profile.displayName) {
+  //           newUser.facebook.name = profile.displayName;
+  //         } else if (profile.name && profile.name.givenName && profile.name.familyName) {
+  //           newUser.facebook.name  = `${profile.name.givenName}  ${profile.name.familyName}`;
+  //         } else {
+  //           newUser.facebook.name = `Facebook user ${profile.id}`;
+  //         }
+
+  //         newUser.save(function(err) {
+  //           if (err) throw err;
+  //           return done(null, newUser);
+  //         });
+  //       }
+  //     });
+  //   });
+  // }));
+}
